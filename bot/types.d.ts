0e498790d41561671892876d0e147230bc32b013
Syth-1 <Syth49@gmail.com>
2024-09-01 23:16:10 +0100

    adjusted types

--- bot/types.d.ts
+++ bot/types.d.ts
@@ -1,81 +1,86 @@
+import type { ModuleLoader } from "./module_loader"
 
-type CommandMap = { [key: string] : CommandFunction }
+declare global {
+    type CommandMap = { [key: string] : CommandFunction }
 
-interface CommandsCollection { 
-    [key: string] : (Commands | NestedCommandObj)
-}
-
-type Commands = {
-    cls : Class,
-    command : CommandFunction
-}
-
-type CommandFunction = (ctx : Context, ...args : unknown) => any
-
-type NestedCommandObj = {
-    onCommandNotFound? : {
-        cls : Class,
-        command : (commandName : string) => void,
+    interface CommandsCollection { 
+        [key: string] : (Commands | NestedCommandObj)
     }
-    onDefaultCommand? : {
-        cls : Class, 
-        command : (ctx : Context) => any
-    },
-    commands : CommandsCollection
+
+    type Commands = {
+        cls : Class,
+        command : CommandFunction
+    }
+
+    type CommandFunction = (ctx : Context, ...args : unknown) => any
+
+    type NestedCommandObj = {
+        onCommandNotFound? : {
+            cls : Class,
+            command : (commandName : string) => void,
+        }
+        onDefaultCommand? : {
+            cls : Class, 
+            command : (ctx : Context) => any
+        },
+        commands : CommandsCollection
+    }
+
+    type CommandBufferMap = Map<Class, CommandMap>
+
+    type Class = { new(...args: any[]): any; };
+
+    interface Context { 
+        msg : string
+        content : string
+
+        methodName : string
+        className  : string
+        globals : Globals
+    }
+
+    interface Globals {
+        botName: string;
+        prefix : string;
+        moduleFolder : string;
+        moduleLoader : ModuleLoader
+        callEvent : (...args: any[]) => any
+    } 
+
+    interface ModuleEvent {
+        [key: string] : Array<Function>
+    }
+
+    interface ListenerEvents {
+        [file: string] : ModuleEvent
+    }
+
+    type Complete<T> = {
+        [P in keyof Required<T>]: Pick<T, P> extends Required<Pick<T, P>> ? T[P] : (T[P] | undefined);
+    }
+
+    type AtLeastOne<T, K extends keyof T = keyof T> = 
+        K extends keyof T 
+        ? { [P in K]-?: T[K] } & Partial<T> 
+        : never;
+
+    type NonEmptyArray<T> = [T, ...Array<T>]
+
+    type KeysOfType<T, Type> = {
+        [K in keyof T]: T[K] extends type ? K : never;
+    }[keyof T];
+
+    type MergeTwo<A, B> = {
+        [K in keyof A | keyof B]: K extends keyof A ? A[K] : K extends keyof B ? B[K] : never;
+    };
+    
+    // Recursive type to merge an array of interfaces
+    type MergeAll<T extends any[]> = T extends [infer Head, ...infer Tail]
+    ? MergeTwo<Head, MergeAll<Tail>>
+    : {};
+
+    type Converter<T, FromType, ToType> = {
+        [K in keyof T]: T[K] extends FromType ? ToType : T[K];
+    };
 }
-
-type CommandBufferMap = Map<Class, CommandMap>
-
-type Class = { new(...args: any[]): any; };
-
-interface Context { 
-    msg : string
-    content : string
-
-    methodName : string
-    className  : string
-}
-
-interface Globals {
-    botName: string;
-    prefix : string;
-    moduleFolder : string;
-    moduleLoader : ModuleLoader
-    callEvent : (...args: any[]) => any
-} 
-
-interface ModuleEvent {
-    [key: string] : Array<Function>
-}
-
-interface ListenerEvents {
-    [file: string] : ModuleEvent
-}
-
-type Complete<T> = {
-    [P in keyof Required<T>]: Pick<T, P> extends Required<Pick<T, P>> ? T[P] : (T[P] | undefined);
-}
-
-type AtLeastOne<T, K extends keyof T = keyof T> = 
-    K extends keyof T 
-    ? { [P in K]-?: T[K] } & Partial<T> 
-    : never;
-
-type NonEmptyArray<T> = [T, ...Array<T>]
-
-type KeysOfType<T, Type> = {
-    [K in keyof T]: T[K] extends type ? K : never;
-}[keyof T];
-
-type MergeTwo<A, B> = {
-    [K in keyof A | keyof B]: K extends keyof A ? A[K] : K extends keyof B ? B[K] : never;
-};
-  
-// Recursive type to merge an array of interfaces
-type MergeAll<T extends any[]> = T extends [infer Head, ...infer Tail]
-? MergeTwo<Head, MergeAll<Tail>>
-: {};
-
-type Converter<T, FromType, ToType> = {
-    [K in keyof T]: T[K] extends FromType ? ToType : T[K];
-};
+export {}
